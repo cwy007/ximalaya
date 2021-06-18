18ba1af8308bb5a80dee7c882c0a8a42126fae8c
chenweiyan <chanweiyan007@gmail.com>
2021-06-18 17:18:59 +0800

    this.props.navigation & this.props.route

--- src/navigator/index.tsx
+++ src/navigator/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+  HeaderStyleInterpolators,
+  StackNavigationProp,
+} from '@react-navigation/stack';
 import Home from '@/pages/Home';
 import Detail from '@/pages/Detail';
+import {Platform, StyleSheet} from 'react-native';
 
-type RootStackParamList = {
+export type RootStackParamList = {
   Home: undefined;
-  Detail: undefined;
+  Detail: {
+    id: number;
+  };
 };
+
+export type RootStackNavigation = StackNavigationProp<RootStackParamList>;
 
 let Stack = createStackNavigator<RootStackParamList>();
 
@@ -16,8 +26,21 @@ class Navigator extends React.Component {
     return (
       <NavigationContainer>
         <Stack.Navigator
+          headerMode="float"
           screenOptions={{
             headerTitleAlign: 'center',
+            headerStyleInterpolator: HeaderStyleInterpolators.forUIKit, // 标题栏动画
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS, // 内容部分的动画
+            gestureEnabled: true,
+            gestureDirection: 'horizontal',
+            headerStyle: {
+              ...Platform.select({
+                android: {
+                  elevation: 0,
+                  borderBottomWidth: StyleSheet.hairlineWidth,
+                },
+              }),
+            },
           }}>
           <Stack.Screen
             options={{headerTitle: '首页'}}
